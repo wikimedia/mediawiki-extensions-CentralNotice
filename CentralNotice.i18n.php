9a3db977b57265d9055ce22bb4828ed907eee540
Huji <huji@users.mediawiki.org>
2008-02-20 22:57:29 +0000

    Adding/updating Persian translations

--- CentralNotice.i18n.php
+++ CentralNotice.i18n.php
@@ -11,6 +11,13 @@ $messages['en'] = array(
 	'centralnotice-desc' => 'Adds a central sitenotice',
 );
 
+/** فارسی (فارسی)
+ * @author Huji
+ */
+$messages['fa'] = array(
+	'centralnotice-desc' => 'یک اطلاعیه مرکزی می‌افزاید',
+);
+
 /** German (Deutsch)
  * @author Raimond Spekking
  */
