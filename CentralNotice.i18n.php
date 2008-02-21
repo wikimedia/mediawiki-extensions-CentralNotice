09c0e1c2656032960566afb77dbd712b8a21e763
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-21 20:36:09 +0000

    Localisation updates for extension messages from Betawiki (2008-02-21 21:06 CET)

--- CentralNotice.i18n.php
+++ CentralNotice.i18n.php
@@ -11,11 +11,18 @@ $messages['en'] = array(
 	'centralnotice-desc' => 'Adds a central sitenotice',
 );
 
-/** فارسی (فارسی)
- * @author Huji
+/** Arabic (العربية)
+ * @author Meno25
  */
-$messages['fa'] = array(
-	'centralnotice-desc' => 'یک اطلاعیه مرکزی می‌افزاید',
+$messages['ar'] = array(
+	'centralnotice-desc' => 'يضيف إعلانا مركزيا للموقع',
+);
+
+/** Czech (Česky)
+ * @author Li-sung
+ */
+$messages['cs'] = array(
+	'centralnotice-desc' => 'Přidává centrální zprávu do záhlaví',
 );
 
 /** German (Deutsch)
@@ -25,10 +32,101 @@ $messages['de'] = array(
 	'centralnotice-desc' => "Fügt eine zentrale ''sitenotice'' hinzu",
 );
 
+/** فارسی (فارسی)
+ * @author Huji
+ */
+$messages['fa'] = array(
+	'centralnotice-desc' => 'یک اطلاعیه مرکزی می‌افزاید',
+);
+
+/** French (Français)
+ * @author Meithal
+ */
+$messages['fr'] = array(
+	'centralnotice-desc' => 'Ajoute un sitenotice central',
+);
+
+/** Franco-Provençal (Arpetan)
+ * @author ChrisPtDe
+ */
+$messages['frp'] = array(
+	'centralnotice-desc' => 'Apond un sitenotice centrâl.',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'centralnotice-desc' => 'Přidawa centralnu bóčnu zdźělenku',
+);
+
+/** Japanese (日本語)
+ * @author JtFuruhata
+ */
+$messages['ja'] = array(
+	'centralnotice-desc' => '中央上部に表示される、サイトからのお知らせを追加する',
+);
+
 /** Dutch (Nederlands)
  * @author Siebrand
  */
 $messages['nl'] = array(
 	'centralnotice-desc' => 'Voegt een centrale sitemededeling toe',
+);
+
+/** Norwegian (‪Norsk (bokmål)‬)
+ * @author Jon Harald Søby
+ */
+$messages['no'] = array(
+	'centralnotice-desc' => 'Legger til en sentral sidenotis',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'centralnotice-desc' => 'Dodaje główny komunikat dla serwisu',
+);
+
+/** Portuguese (Português)
+ * @author Malafaya
+ */
+$messages['pt'] = array(
+	'centralnotice-desc' => 'Adiciona um aviso do sítio centralizado',
+);
+
+/** Russian (Русский)
+ * @author .:Ajvol:.
+ */
+$messages['ru'] = array(
+	'centralnotice-desc' => 'Добавляет общее сообщение сайта',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'centralnotice-desc' => 'Pridáva centrálnu Správu lokality',
+);
+
+/** Swedish (Svenska)
+ * @author M.M.S.
+ */
+$messages['sv'] = array(
+	'centralnotice-desc' => 'Lägger till en central sidnotis',
+);
+
+/** Telugu (తెలుగు)
+ * @author Chaduvari
+ */
+$messages['te'] = array(
+	'centralnotice-desc' => 'కేంద్రీకృత సైటు నోటీసు ఒకటి పెట్టండి',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ */
+$messages['vi'] = array(
+	'centralnotice-desc' => 'Thêm thông báo ở đầu các trang tại hơn một wiki',
 );
 
