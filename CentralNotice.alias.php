5b11e6e06efdef9a8e43819a8d65348ac32992c3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-05-17 18:29:36 +0000

    Localisation updates for extension special page aliases and magic words from translatewiki.net * some non-essential language header eye candy was removed because these exports were made with the (slow!) magic-export.php, and not with the broken alias-export.php

--- CentralNotice.alias.php
+++ CentralNotice.alias.php
@@ -11,3 +11,20 @@ $aliases['en'] = array(
 	'NoticeTemplate' => array( 'NoticeTemplate' ),
 );
 
+$aliases['ar'] = array(
+	'CentralNotice'    => array( 'ملاحظة_مركزية' ),
+	'NoticeText'       => array( 'نص_الملاحظة' ),
+	'NoticeTemplate'   => array( 'قالب_الملاحظة' ),
+);
+
+$aliases['ja'] = array(
+	'CentralNotice'    => array( '中央管理通知' ),
+	'NoticeText'       => array( '通知文' ),
+	'NoticeTemplate'   => array( '通知テンプレート' ),
+);
+
+$aliases['ml'] = array(
+	'CentralNotice'    => array( 'കേന്ദ്രീകൃതഅറിയിപ്പ്' ),
+	'NoticeText'       => array( 'അറിയിപ്പ്‌‌എഴുത്ത്' ),
+	'NoticeTemplate'   => array( 'അറിയിപ്പ്ഫലകം' ),
+);
