4f0a471a1a47f13717da4181351ab3ab5d08c603
Raimond Spekking <raymond@users.mediawiki.org>
2010-05-18 20:00:55 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-05-18 19:45:00 UTC)

--- CentralNotice.alias.php
+++ CentralNotice.alias.php
@@ -11,20 +11,24 @@ $aliases['en'] = array(
 	'NoticeTemplate' => array( 'NoticeTemplate' ),
 );
 
+/** Arabic (العربية) */
 $aliases['ar'] = array(
-	'CentralNotice'    => array( 'ملاحظة_مركزية' ),
-	'NoticeText'       => array( 'نص_الملاحظة' ),
-	'NoticeTemplate'   => array( 'قالب_الملاحظة' ),
+	'CentralNotice' => array( 'ملاحظة_مركزية' ),
+	'NoticeText' => array( 'نص_الملاحظة' ),
+	'NoticeTemplate' => array( 'قالب_الملاحظة' ),
 );
 
+/** Japanese (日本語) */
 $aliases['ja'] = array(
-	'CentralNotice'    => array( '中央管理通知' ),
-	'NoticeText'       => array( '通知文' ),
-	'NoticeTemplate'   => array( '通知テンプレート' ),
+	'CentralNotice' => array( '中央管理通知' ),
+	'NoticeText' => array( '通知文' ),
+	'NoticeTemplate' => array( '通知テンプレート' ),
 );
 
+/** Malayalam (മലയാളം) */
 $aliases['ml'] = array(
-	'CentralNotice'    => array( 'കേന്ദ്രീകൃതഅറിയിപ്പ്' ),
-	'NoticeText'       => array( 'അറിയിപ്പ്‌‌എഴുത്ത്' ),
-	'NoticeTemplate'   => array( 'അറിയിപ്പ്ഫലകം' ),
+	'CentralNotice' => array( 'കേന്ദ്രീകൃതഅറിയിപ്പ്' ),
+	'NoticeText' => array( 'അറിയിപ്പ്‌‌എഴുത്ത്' ),
+	'NoticeTemplate' => array( 'അറിയിപ്പ്ഫലകം' ),
 );
+
