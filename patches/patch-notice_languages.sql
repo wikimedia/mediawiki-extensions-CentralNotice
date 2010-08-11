2811a148dd6d37efde1237d71ff97fec9fd20cb6
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-08-11 18:53:11 +0000

    moving patch-add-preferred.sql prior to adding to update hook

--- patches/patch-notice_languages.sql
+++ patches/patch-notice_languages.sql
@@ -1,9 +1,8 @@
 -- Update to allow for any number of languages per notice.
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cn_notice_languages (
-  `id` int unsigned NOT NULL auto_increment,
+  `id` int unsigned NOT NULL auto_increment PRIMARY KEY,
   `not_id` int unsigned NOT NULL,
-  `not_language` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `not_id_not_language` (`not_id`,`not_language`)
+  `not_language` varchar(32) NOT NULL
 ) /*$wgDBTableOptions*/;
+CREATE INDEX /*i*/cn_not_id_not_language ON /*$wgDBprefix*/cn_notice_languages (not_id, not_language);
