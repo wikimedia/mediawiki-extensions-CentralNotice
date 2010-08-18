024841ec9004e0f21f3ff3ed0ec626c2266c4b94
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-08-18 00:08:06 +0000

    forgot to update main SQL file for r71004, cleaning up SQL formatting

--- CentralNotice.sql
+++ CentralNotice.sql
@@ -1,30 +1,32 @@
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cn_notices (
-  `not_id` int NOT NULL PRIMARY KEY auto_increment,
-  `not_name` varchar(255) NOT NULL,
-  `not_start` char(14) NOT NULL,
-  `not_end` char(14) NOT NULL,
-  `not_enabled` bool NOT NULL default '0',
-  `not_preferred` bool NOT NULL default '0',
-  `not_locked` bool NOT NULL default '0',
-  `not_language` varchar(32) NOT NULL,
-  `not_project` varchar(255) NOT NULL
+	`not_id` int NOT NULL PRIMARY KEY auto_increment,
+	`not_name` varchar(255) NOT NULL,
+	`not_start` char(14) NOT NULL,
+	`not_end` char(14) NOT NULL,
+	`not_enabled` bool NOT NULL default '0',
+	`not_preferred` bool NOT NULL default '0',
+	`not_locked` bool NOT NULL default '0',
+	`not_language` varchar(32) NOT NULL,
+	`not_project` varchar(255) NOT NULL
 ) /*$wgDBTableOptions*/;
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cn_assignments (
-  `asn_id` int NOT NULL PRIMARY KEY auto_increment,
-  `not_id` int NOT NULL,
-  `tmp_id` int NOT NULL,
-  `tmp_weight` int NOT NULL
+	`asn_id` int NOT NULL PRIMARY KEY auto_increment,
+	`not_id` int NOT NULL,
+	`tmp_id` int NOT NULL,
+	`tmp_weight` int NOT NULL
 ) /*$wgDBTableOptions*/;
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cn_templates (
-  `tmp_id` int NOT NULL PRIMARY KEY auto_increment,
-  `tmp_name` varchar(255) default NULL
+	`tmp_id` int NOT NULL PRIMARY KEY auto_increment,
+	`tmp_name` varchar(255) default NULL
+	`tmp_display_anon` BOOLEAN NOT NULL DEFAULT 1,
+	`tmp_display_account` BOOLEAN NOT NULL DEFAULT 1;
 ) /*$wgDBTableOptions*/;
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cn_notice_languages (
-  `id` int unsigned NOT NULL PRIMARY KEY auto_increment,
-  `not_id` int unsigned NOT NULL,
-  `not_language` varchar(32) NOT NULL
+	`id` int unsigned NOT NULL PRIMARY KEY auto_increment,
+	`not_id` int unsigned NOT NULL,
+	`not_language` varchar(32) NOT NULL
 ) /*$wgDBTableOptions*/;
 CREATE UNIQUE INDEX /*i*/cn_not_id_not_language ON /*$wgDBprefix*/cn_notice_languages (not_id, not_language);
--- patches/patch-notice_languages.sql
+++ patches/patch-notice_languages.sql
@@ -1,8 +1,8 @@
 -- Update to allow for any number of languages per notice.
 
 CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cn_notice_languages (
-  `id` int unsigned NOT NULL PRIMARY KEY auto_increment,
-  `not_id` int unsigned NOT NULL,
-  `not_language` varchar(32) NOT NULL
+	`id` int unsigned NOT NULL PRIMARY KEY auto_increment,
+	`not_id` int unsigned NOT NULL,
+	`not_language` varchar(32) NOT NULL
 ) /*$wgDBTableOptions*/;
 CREATE UNIQUE INDEX /*i*/cn_not_id_not_language ON /*$wgDBprefix*/cn_notice_languages (not_id, not_language);
