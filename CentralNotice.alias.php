e04842d553f242cfcad5f53e04ca5ed61a27f6e8
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-08-24 14:06:50 +0000

    Localisation updates for extensions from translatewiki.net * update special page aliases.

--- CentralNotice.alias.php
+++ CentralNotice.alias.php
@@ -12,24 +12,66 @@ $aliases['en'] = array(
 	'NoticeLocal' => array( 'NoticeLocal' ),
 );
 
-/** Arabic (العربية) */
 $aliases['ar'] = array(
-	'CentralNotice' => array( 'ملاحظة_مركزية' ),
-	'NoticeText' => array( 'نص_الملاحظة' ),
-	'NoticeTemplate' => array( 'قالب_الملاحظة' ),
+	'CentralNotice'    => array( 'ملاحظة_مركزية' ),
+	'NoticeText'       => array( 'نص_الملاحظة' ),
+	'NoticeTemplate'   => array( 'قالب_الملاحظة' ),
 );
 
-/** Japanese (日本語) */
+$aliases['arz'] = array(
+	'CentralNotice'    => array( 'ملاحظة_مركزية' ),
+	'NoticeText'       => array( 'نص_الملاحظة' ),
+	'NoticeTemplate'   => array( 'قالب_الملاحظة' ),
+);
+
+$aliases['fa'] = array(
+	'CentralNotice'    => array( 'اعلامیه_مرکزی' ),
+	'NoticeText'       => array( 'متن_اعلامیه' ),
+	'NoticeTemplate'   => array( 'الگوی_اعلامیه' ),
+	'NoticeLocal'      => array( 'اعلامیه_محلی' ),
+);
+
 $aliases['ja'] = array(
-	'CentralNotice' => array( '中央管理通知' ),
-	'NoticeText' => array( '通知文' ),
-	'NoticeTemplate' => array( '通知テンプレート' ),
+	'CentralNotice'    => array( '中央管理通知' ),
+	'NoticeText'       => array( '通知文' ),
+	'NoticeTemplate'   => array( '通知テンプレート' ),
 );
 
-/** Malayalam (മലയാളം) */
+$aliases['lad'] = array(
+	'CentralNotice'    => array( 'AvisoCentral' ),
+	'NoticeText'       => array( 'Teksto_de_aviso' ),
+	'NoticeTemplate'   => array( 'Xabblón_de_aviso' ),
+	'NoticeLocal'      => array( 'AvisoLocal' ),
+);
+
 $aliases['ml'] = array(
-	'CentralNotice' => array( 'കേന്ദ്രീകൃതഅറിയിപ്പ്' ),
-	'NoticeText' => array( 'അറിയിപ്പ്‌‌എഴുത്ത്' ),
-	'NoticeTemplate' => array( 'അറിയിപ്പ്ഫലകം' ),
+	'CentralNotice'    => array( 'കേന്ദ്രീകൃതഅറിയിപ്പ്' ),
+	'NoticeText'       => array( 'അറിയിപ്പ്‌‌എഴുത്ത്' ),
+	'NoticeTemplate'   => array( 'അറിയിപ്പ്ഫലകം' ),
+	'NoticeLocal'      => array( 'പ്രാദേശിക‌‌അറിയിപ്പ്' ),
 );
 
+$aliases['nn'] = array(
+	'CentralNotice'    => array( 'Sentralmerknad' ),
+	'NoticeText'       => array( 'Merknadstekst' ),
+	'NoticeTemplate'   => array( 'Merknadsmal' ),
+);
+
+$aliases['no'] = array(
+	'CentralNotice'    => array( 'Sentralnotis' ),
+	'NoticeText'       => array( 'Notistekst' ),
+	'NoticeTemplate'   => array( 'Notismal' ),
+);
+
+$aliases['pl'] = array(
+	'CentralNotice'    => array( 'Globalny_komunikat' ),
+	'NoticeText'       => array( 'Treść_komunikatu' ),
+	'NoticeTemplate'   => array( 'Szablon_komunikatu' ),
+);
+
+$aliases['zh-hant'] = array(
+	'CentralNotice'    => array( '中央通告' ),
+	'NoticeText'       => array( '通告內文' ),
+	'NoticeTemplate'   => array( '通告模板' ),
+	'NoticeLocal'      => array( '本地化通告' ),
+);
