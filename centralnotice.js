08dd1568164c629afd660c058f5177106ea280b1
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-09-10 23:07:27 +0000

    beginning to add geotargetting interface

--- centralnotice.js
+++ centralnotice.js
@@ -43,3 +43,15 @@ function insertButton( buttonType ) {
 	}
 	bannerField.focus();
 }
+// Handle revealing the geoMultiSelector when the geotargetted checkbox is checked
+( function( $ ) {
+	$(document).ready(function() {
+		$("#geotargeted").click(function () {
+			if ($('#geotargeted:checked').val() !== undefined) {
+				$("#geoMultiSelector").show('slow');
+			} else {
+				$("#geoMultiSelector").hide('slow');
+			}
+		});
+	});
+})(jQuery);
