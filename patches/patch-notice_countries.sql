96c55c87871048b2f76263cd34dc7a06ea15f086
Ryan Kaldari <kaldari@users.mediawiki.org>
2010-09-21 00:49:19 +0000

    mySQL 4 support

--- patches/patch-notice_countries.sql
+++ patches/patch-notice_countries.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/cn_notice_countries (
 	nc_country varchar(2) NOT NULL
 ) /*$wgDBTableOptions*/;
 CREATE UNIQUE INDEX /*i*/nc_notice_id_country ON /*$wgDBprefix*/cn_notice_countries (nc_notice_id, nc_country);
-ALTER TABLE /*$wgDBprefix*/cn_notices ADD not_geo BOOLEAN NOT NULL DEFAULT '0' AFTER not_locked; 
+ALTER TABLE /*$wgDBprefix*/cn_notices ADD not_geo bool NOT NULL DEFAULT '0' AFTER not_locked;
