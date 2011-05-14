5cd73257bab12f71ad39f1abdf2fc241d735d2f6
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-05-14 00:11:22 +0000

    Localisation updates for special page aliases for extensions from http://translatewiki.net.

--- CentralNotice.alias.php
+++ CentralNotice.alias.php
@@ -20,6 +20,11 @@ $specialPageAliases['en'] = array(
 $specialPageAliases['ar'] = array(
 	'CentralNotice' => array( 'ملاحظة_مركزية' ),
 	'NoticeTemplate' => array( 'قالب_الملاحظة' ),
+	'BannerAllocation' => array( 'وضع_الإعلان' ),
+	'BannerController' => array( 'متحكم_الإعلان' ),
+	'BannerListLoader' => array( 'محمل_قائمة_الإعلان' ),
+	'BannerLoader' => array( 'محمل_الإعلان' ),
+	'HideBanners' => array( 'إخفاء_الإعلان' ),
 );
 
 /** Egyptian Spoken Arabic (مصرى) */
@@ -28,10 +33,47 @@ $specialPageAliases['arz'] = array(
 	'NoticeTemplate' => array( 'قالب_الملاحظة' ),
 );
 
+/** Breton (Brezhoneg) */
+$specialPageAliases['br'] = array(
+	'HideBanners' => array( 'KuzhatBanniel' ),
+);
+
+/** Esperanto (Esperanto) */
+$specialPageAliases['eo'] = array(
+	'CentralNotice' => array( 'Centra_informilo' ),
+	'NoticeTemplate' => array( 'Informŝablono' ),
+	'BannerAllocation' => array( 'Strirezervado' ),
+	'BannerController' => array( 'Informstrikontrolisto' ),
+	'BannerListLoader' => array( 'Informstrilistoŝargilo' ),
+	'BannerLoader' => array( 'Informstriŝargilo' ),
+	'HideBanners' => array( 'Kaŝu_striojn' ),
+);
+
 /** Persian (فارسی) */
 $specialPageAliases['fa'] = array(
 	'CentralNotice' => array( 'اعلامیه_مرکزی' ),
 	'NoticeTemplate' => array( 'الگوی_اعلامیه' ),
+);
+
+/** 湘语 (湘语) */
+$specialPageAliases['hsn'] = array(
+	'CentralNotice' => array( '中心公告' ),
+	'NoticeTemplate' => array( '公告样范' ),
+	'BannerAllocation' => array( '横幅配置' ),
+	'BannerController' => array( '横幅控制器' ),
+	'BannerListLoader' => array( '横幅清单载入器' ),
+	'BannerLoader' => array( '横幅载入器' ),
+	'HideBanners' => array( '隐藏横幅' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'CentralNotice' => array( 'NòtSantral' ),
+	'NoticeTemplate' => array( 'ModèlNòt' ),
+	'BannerAllocation' => array( 'BayAnsey' ),
+	'BannerController' => array( 'KontroleAnsey' ),
+	'BannerListLoader' => array( 'ChajeLisAnsey' ),
+	'BannerLoader' => array( 'ChajeAnsey' ),
 );
 
 /** Interlingua (Interlingua) */
@@ -52,12 +94,24 @@ $specialPageAliases['ja'] = array(
 	'BannerController' => array( 'テンプレート制御' ),
 	'BannerListLoader' => array( 'テンプレート一覧読み込み' ),
 	'BannerLoader' => array( 'テンプレート読み込み' ),
+	'HideBanners' => array( 'バナーを隠す' ),
 );
 
 /** Ladino (Ladino) */
 $specialPageAliases['lad'] = array(
 	'CentralNotice' => array( 'AvisoCentral' ),
 	'NoticeTemplate' => array( 'Xabblón_de_aviso' ),
+);
+
+/** Macedonian (Македонски) */
+$specialPageAliases['mk'] = array(
+	'CentralNotice' => array( 'ЦентралноИзвестување' ),
+	'NoticeTemplate' => array( 'ШаблонЗаИзвестување' ),
+	'BannerAllocation' => array( 'РаспределбаНаПлакати' ),
+	'BannerController' => array( 'КонтролорНаПлакати' ),
+	'BannerListLoader' => array( 'ВчитувачНаСписоциНаПлакати' ),
+	'BannerLoader' => array( 'ВчитувачНаПлакати' ),
+	'HideBanners' => array( 'СкријПлакати' ),
 );
 
 /** Malayalam (മലയാളം) */
@@ -74,6 +128,7 @@ $specialPageAliases['nl'] = array(
 	'BannerController' => array( 'Bannerbeheerder' ),
 	'BannerListLoader' => array( 'Bannerlijstlader' ),
 	'BannerLoader' => array( 'Bannerlader' ),
+	'HideBanners' => array( 'BannersVerbergen' ),
 );
 
 /** Norwegian Nynorsk (‪Norsk (nynorsk)‬) */
@@ -86,12 +141,49 @@ $specialPageAliases['nn'] = array(
 $specialPageAliases['no'] = array(
 	'CentralNotice' => array( 'Sentralnotis' ),
 	'NoticeTemplate' => array( 'Notismal' ),
+	'BannerAllocation' => array( 'Bannerplassering' ),
+	'BannerController' => array( 'Bannerkontroll' ),
+	'BannerListLoader' => array( 'Bannerlistelaster' ),
+	'BannerLoader' => array( 'Bannerlaster' ),
 );
 
 /** Polish (Polski) */
 $specialPageAliases['pl'] = array(
 	'CentralNotice' => array( 'Globalny_komunikat' ),
 	'NoticeTemplate' => array( 'Szablon_komunikatu' ),
+);
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = array(
+	'CentralNotice' => array( 'MerkeziBildirim' ),
+	'NoticeTemplate' => array( 'BildirimŞablonu' ),
+	'BannerAllocation' => array( 'AfişTahsisi' ),
+	'BannerController' => array( 'AfişKontrolü', 'AfişKontrolAracı' ),
+	'BannerListLoader' => array( 'AfişListeYükleyici' ),
+	'BannerLoader' => array( 'AfişYükleyici' ),
+	'HideBanners' => array( 'AfişleriGizle', 'AfişGizle' ),
+);
+
+/** Vietnamese (Tiếng Việt) */
+$specialPageAliases['vi'] = array(
+	'CentralNotice' => array( 'Thông_báo_chung' ),
+	'NoticeTemplate' => array( 'Bản_mẫu_thông_báo' ),
+	'BannerAllocation' => array( 'Phân_bố_bảng' ),
+	'BannerController' => array( 'Điều_khiển_bảng' ),
+	'BannerListLoader' => array( 'Tải_danh_sách_bảng' ),
+	'BannerLoader' => array( 'Tải_bảng' ),
+	'HideBanners' => array( 'Ẩn_bảng' ),
+);
+
+/** Simplified Chinese (‪中文(简体)‬) */
+$specialPageAliases['zh-hans'] = array(
+	'CentralNotice' => array( '中央通知' ),
+	'NoticeTemplate' => array( '公告模板' ),
+	'BannerAllocation' => array( '横幅分配' ),
+	'BannerController' => array( '横幅控制器' ),
+	'BannerListLoader' => array( '布条装载机' ),
+	'BannerLoader' => array( '横幅装载机' ),
+	'HideBanners' => array( '隐藏横幅' ),
 );
 
 /** Traditional Chinese (‪中文(繁體)‬) */
