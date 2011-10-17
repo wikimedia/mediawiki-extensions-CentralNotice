045a8782116c131b9d7dfdad069a48494f6a80d2
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-10-17 21:00:03 +0000

    follow-up to r99160, indexing banner names

--- patches/patch-template_autolink.sql
+++ patches/patch-template_autolink.sql
@@ -6,3 +6,6 @@ ALTER TABLE /*$wgDBprefix*/cn_templates ADD `tmp_autolink` bool NOT NULL DEFAULT
 -- Store before and after flag values for logging
 ALTER TABLE /*$wgDBprefix*/cn_template_log ADD `tmplog_begin_autolink` bool NULL DEFAULT NULL AFTER `tmplog_end_fundraising`;
 ALTER TABLE /*$wgDBprefix*/cn_template_log ADD `tmplog_end_autolink` bool NULL DEFAULT NULL AFTER `tmplog_begin_autolink`;
+
+-- Add an index for banner names since we sometimes use them for selection
+CREATE INDEX /*i*/tmp_name ON /*_*/cn_templates (tmp_name);
