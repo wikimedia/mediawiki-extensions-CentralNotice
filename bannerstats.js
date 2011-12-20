d078c4a68f57dd42071df3529c1e75c14f478394
Ryan Kaldari <kaldari@users.mediawiki.org>
2011-12-20 21:54:33 +0000

    make sure we dont throw a js error when no campaigns are running

--- bannerstats.js
+++ bannerstats.js
@@ -4,22 +4,24 @@
  */
 $( document ).ready( function () {
 	/*
-	$.each( wgCentralNoticeAllocationCampaigns, function( index, campaignName ) {
-		var statUrl = 'http://fundraising-analytics.wikimedia.org/live_stats/' + campaignName;
-		$.ajax( {
-			'url': statUrl,
-			'data': {},
-			'dataType': 'jsonp',
-			'type': 'GET',
-			'success': function( data ) {
-				//console.debug( "Success" );
-				//console.debug( data );
-			},
-			'error': function( xhr ) {
-				//console.debug( "Error" );
-				//console.debug( xhr );
-			}
-		} );
-	} )
+	if ( typeof wgCentralNoticeAllocationCampaigns !== 'undefined' ) {
+		$.each( wgCentralNoticeAllocationCampaigns, function( index, campaignName ) {
+			var statUrl = 'http://fundraising-analytics.wikimedia.org/json_reporting/' + campaignName;
+			$.ajax( {
+				'url': statUrl,
+				'data': {},
+				'dataType': 'jsonp',
+				'type': 'GET',
+				'success': function( data ) {
+					//console.debug( "Success" );
+					//console.debug( data );
+				},
+				'error': function( xhr ) {
+					//console.debug( "Error" );
+					//console.debug( xhr );
+				}
+			} );
+		} )
+	}
 	*/
 } );
