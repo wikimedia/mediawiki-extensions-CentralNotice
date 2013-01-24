2fbbcf6da55c79449d258994e51cc79ad61a9f8d
Matthew Walker <mwalker@wikimedia.org>
2013-01-24 14:17:02 -0800

    Ahhh Fix CentralNotice SQL for Deployment
    The patch file -- though it would have worked should have had
    only one modification per table.
    
    Change-Id: I72f709567f53fa447c7537d4c21bf12a123abb5e

--- patches/patch-prioritylangs.sql
+++ patches/patch-prioritylangs.sql
@@ -1,5 +1,7 @@
 -- We now support the translate extension; but we needed an ability to promote languages over
 -- other ones in the interface. Though this is handled in the translate extension itself, we still
 -- need to log it.
-ALTER TABLE /*$wgDBprefix*/cn_template_log ADD COLUMN tmplog_begin_prioritylangs text default '';
-ALTER TABLE /*$wgDBprefix*/cn_template_log ADD COLUMN tmplog_end_prioritylangs text default '';
+ALTER TABLE /*$wgDBprefix*/cn_template_log ADD (
+  tmplog_begin_prioritylangs text default '',
+  tmplog_end_prioritylangs text default ''
+);
