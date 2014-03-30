c6701560a4952d9bcdc2ff6260c9d14df3b5dc71
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-30 23:37:06 +0200

    Add the B/C i18n shim
    I mistakenly was under the impression that CentralNotice was only used by
    WMF wikis. Adam Wight pointed this out. This is a follow-up to
    06df9c0de7d5a309466f6eb73b5c48a9092be0e2.
    
    Change-Id: Ie6a4a95fdac6f71e55cc7bf1cccf0eb7565b9759

--- CentralNotice.php
+++ CentralNotice.php
@@ -225,7 +225,8 @@ require_once $dir . '/CentralNotice.hooks.php';
 require_once $dir . '/CentralNotice.modules.php';
 
 // Register message files
-$wgMessagesDirs['CentralNotice'] = __DIR__ . '/i18n';
+$wgMessagesDirs['SpecialCentralAuth'] = __DIR__ . '/i18n';
+$wgExtensionMessagesFiles['SpecialCentralAuth'] = __DIR__ . "/CentralAuth.i18n.php";
 $wgExtensionMessagesFiles[ 'CentralNoticeAliases' ] = $dir . '/CentralNotice.alias.php';
 
 // Register user rights
