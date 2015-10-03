e85d26161750debfefd061037cd62f6f1c791da5
Paladox <thomasmulhall410@yahoo.com>
2015-10-03 12:03:33 +0100

    Add npm support, Add composer.json
    This will allow us to run npm test.
    
    It will also allow us to run php code sniffer in the future.
    
    Also add missing qqq.json messages.
    
    Change-Id: I75573c55c716190d794599d4fd0736cdff224d03

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js'
+			]
+		},
+		banana: {
+			all: 'i18n/'
+		},
+		jsonlint: {
+			all: [
+				'*.json',
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
