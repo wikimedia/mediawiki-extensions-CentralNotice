4a7f60f506b3ab4d8b9528c4bce60ebefe85cb4c
Umherirrender <umherirrender_de.wp@web.de>
2018-10-18 21:22:52 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: If98f3263c7ed49017ceaf8da76a738b84460bdc2

--- CentralNotice.php
+++ CentralNotice.php
@@ -15,12 +15,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['CentralNotice'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles[ 'CentralNoticeAliases' ] = __DIR__ . '/CentralNotice.alias.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for CentralNotice extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the CentralNotice extension requires MediaWiki 1.25+' );
+	die( 'This version of the CentralNotice extension requires MediaWiki 1.31+' );
 }
