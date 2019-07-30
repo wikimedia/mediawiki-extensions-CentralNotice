26b164d3675dfb477a4227456775c886af75fb91
Andrew Green <andrew.green.df@gmail.com>
2019-07-30 18:24:44 -0400

    Fix and reorganize QUnit tests
    Move QUnit tests to reflect location of code under test, and update
    method of registering test modules to use QUnitTestModule in
    extension.json.
    
    Bug: T227879
    Change-Id: Ie02bec2665639fa101bd016edf8673eb3dab981f

---
+++ tests/qunit/ext.centralNotice.kvStore/kvStore.tests.js
@@ -0,0 +1,30 @@
+( function () {
+	var realIdleCallback = mw.requestIdleCallback;
+
+	QUnit.module( 'ext.centralNotice.kvStore', QUnit.newMwEnvironment( {
+		teardown: function () {
+			var key, i = localStorage.length;
+			// Loop backwards since removal affects the key index,
+			// causing items to consistently be skipped over
+			while ( i-- > 0 ) {
+				key = localStorage.key( i );
+				if ( /^CentralNoticeKV.+\|unittest/.test( key ) ) {
+					localStorage.removeItem( key );
+				}
+			}
+			mw.requestIdleCallback = realIdleCallback;
+		}
+	} ) );
+
+	QUnit.test( 'getItem', function ( assert ) {
+		var kvStore = mw.centralNotice.kvStore,
+			context = kvStore.contexts.GLOBAL;
+		kvStore.setItem( 'unittest-New', 'x', context, 1 );
+		kvStore.setItem( 'unittest-Old', 'x', context, -2 );
+
+		assert.strictEqual( kvStore.getError(), null, 'no errors' );
+		assert.strictEqual( kvStore.getItem( 'unittest-New', context ), 'x', 'retrieve valid item' );
+		// Verify that expiry is verified at run-time regardless of kvStoreMaintenance
+		assert.strictEqual( kvStore.getItem( 'unittest-Old', context ), null, 'ignore expired item' );
+	} );
+}() );
--- tests/qunit/ext.centralNotice.startUp/kvStoreMaintenance.tests.js
+++ tests/qunit/ext.centralNotice.startUp/kvStoreMaintenance.tests.js
@@ -1,7 +1,7 @@
 ( function () {
 	var realIdleCallback = mw.requestIdleCallback;
 
-	QUnit.module( 'ext.centralNotice.kvStore', QUnit.newMwEnvironment( {
+	QUnit.module( 'ext.centralNotice.startUp', QUnit.newMwEnvironment( {
 		teardown: function () {
 			var key, i = localStorage.length;
 			// Loop backwards since removal affects the key index,
@@ -15,18 +15,6 @@
 			mw.requestIdleCallback = realIdleCallback;
 		}
 	} ) );
-
-	QUnit.test( 'getItem', function ( assert ) {
-		var kvStore = mw.centralNotice.kvStore,
-			context = kvStore.contexts.GLOBAL;
-		kvStore.setItem( 'unittest-New', 'x', context, 1 );
-		kvStore.setItem( 'unittest-Old', 'x', context, -2 );
-
-		assert.strictEqual( kvStore.getError(), null, 'no errors' );
-		assert.strictEqual( kvStore.getItem( 'unittest-New', context ), 'x', 'retrieve valid item' );
-		// Verify that expiry is verified at run-time regardless of kvStoreMaintenance
-		assert.strictEqual( kvStore.getItem( 'unittest-Old', context ), null, 'ignore expired item' );
-	} );
 
 	QUnit.test( 'maintenance', function ( assert ) {
 		var kvStore = mw.centralNotice.kvStore,
