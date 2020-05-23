1afcaf79376e05924bf4049a177e838883cf9584
Umherirrender <umherirrender_de.wp@web.de>
2020-05-24 00:34:41 +0200

    Parse messages in string context
    Change-Id: Id886751bfa6c9ebc98ec6df190bf5abc3cb7c51f

--- includes/Banner.php
+++ includes/Banner.php
@@ -1621,7 +1621,7 @@ class Banner {
 		);
 		if ( !$status->isGood() || !$cascade ) {
 			throw new BannerContentException(
-				'Unable to protect banner' . $status->getMessage()
+				'Unable to protect banner' . $status->getMessage()->text()
 			);
 		}
 	}
