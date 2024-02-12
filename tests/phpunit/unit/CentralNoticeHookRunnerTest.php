d2adee73bed572491efde4070e22d89910888790
Umherirrender <umherirrender_de.wp@web.de>
2024-02-12 20:28:45 +0100

    Rename HookRunnerTest
    Unify the name accross the extensions
    
    Bug: T357339
    Change-Id: I9258940b8eb7d911f116dbaf4cfd644bcac4a133

--- tests/phpunit/unit/CentralNoticeHookRunnerTest.php
+++ tests/phpunit/unit/CentralNoticeHookRunnerTest.php
@@ -5,7 +5,7 @@ use MediaWiki\Tests\HookContainer\HookRunnerTestBase;
 /**
  * @covers \CentralNoticeHookRunner
  */
-class HookRunnerTest extends HookRunnerTestBase {
+class CentralNoticeHookRunnerTest extends HookRunnerTestBase {
 
 	public static function provideHookRunners() {
 		yield CentralNoticeHookRunner::class => [ CentralNoticeHookRunner::class ];
