3e6d8736c05ed05cbb40a90ca5e50e4e575e492d
Jack Gleeson <jgleeson@wikimedia.org>
2025-05-19 19:28:19 +0100

    Make BundleSizeTest cross-compatible with <=1.44 and >=1.45
    Looks like CentralNotice should have been on the list of extensions
    being upgraded here https://phabricator.wikimedia.org/T393122
    
    Bug: T394542
    Bug: T394399
    Bug: T393122
    Change-Id: I91416975ff4e6b1d3213ad1d7d198dd45c7ec4bf

--- tests/phpunit/structure/BundleSizeTest.php
+++ tests/phpunit/structure/BundleSizeTest.php
@@ -7,7 +7,15 @@ use MediaWiki\Tests\Structure\BundleSizeTestBase;
 class BundleSizeTest extends BundleSizeTestBase {
 
 	/** @inheritDoc */
-	public function getBundleSizeConfig(): string {
+	public static function getBundleSizeConfigData(): string {
 		return dirname( __DIR__, 3 ) . '/bundlesize.config.json';
 	}
+
+	/**
+	 * @deprecated can be removed when support for MediaWiki 1.44 is dropped
+	 */
+	public function getBundleSizeConfig(): string {
+		return self::getBundleSizeConfigData();
+	}
+
 }
