a7c8a8e8dc7f65ac490a9017778ee0b40ea1cac8
Umherirrender <umherirrender_de.wp@web.de>
2025-09-17 20:07:25 +0200

    build: Handle excluded ClassAnnotations and CommentBeforeClass sniff
    - Split comment and move license part to top
    - Remove @section to make it a file comment
    - Remove spaces before classes
    
    Change-Id: Ie36b8b7bc51cc564d00c0bf5bafb4b53e11182e0

--- includes/BannerDataException.php
+++ includes/BannerDataException.php
@@ -3,7 +3,6 @@
  * This file is part of the CentralNotice Extension to MediaWiki
  * https://www.mediawiki.org/wiki/Extension:CentralNotice
  *
- * @section LICENSE
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
